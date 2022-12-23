30e2c6554ad1b514692f4257544858d95208f040
Hamza384 <hamzaciit133@gmail.com>
2022-12-23 11:06:56 +0500

    Initial commit

---
+++ app/src/main/java/com/example/photopickerandroid13/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.photopickerandroid13
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.photopickerandroid13.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private val MAX_ITEMS = 5 // max number of allowed items to be selected
+
+    private val singleImagePicker =
+        registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            // Callback is invoked after the user selects a media item or closes the
+            // photo picker.
+            uri?.let {
+                val selectedImages = StringBuilder()
+                selectedImages.append(uri.toString())
+                binding.selectedImagesUri.text = selectedImages
+            }
+        }
+
+    private val multipleImagePicker =
+        registerForActivityResult(ActivityResultContracts.PickMultipleVisualMedia(MAX_ITEMS)) { selectedUris ->
+            // Callback is invoked after the user selects media items or closes the
+            if (selectedUris.isNotEmpty()) {
+                val selectedImages = StringBuilder()
+                for (uri in selectedUris) {
+                    selectedImages.append(uri.toString()).append("\n")
+                }
+                binding.selectedImagesUri.text = selectedImages
+            }
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initView()
+    }
+
+    private fun initView() {
+        binding.singleImagePick.setOnClickListener {
+            singleImagePicker.launch(
+                PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+            )
+        }
+
+        /**
+         * you can select only 5 images because of value for MAX_ITEMS
+         * */
+        binding.multiImagePick.setOnClickListener {
+            multipleImagePicker.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
+    }
+}
